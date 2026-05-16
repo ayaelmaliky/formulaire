a1de546b9bb515e5982f193f52c5c24be50d2ac8
Aya Elmaliky <ayaelmalki2004@gmail.com>
2026-05-16 14:50:25 +0200

    Ajout des fichiers

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
-const nom = document.getElementById("nom");
+const nom = document.getElementById("name");
 const email = document.getElementById("email");
 const password = document.getElementById("password");
 const button = document.getElementById("button");
 
-button.addEventListener("click", function(){
+button.addEventListener("click", function(e){
+
+    e.preventDefault();
 
     if(nom.value == "" || email.value == "" || password.value == ""){
         alert("Les champs sont vides");
