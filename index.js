ef7c0460b637dc7240509f7406b96fdc63bf45a9
Aya Elmaliky <ayaelmalki2004@gmail.com>
2026-05-16 14:41:46 +0200

    Ajout des fichiers

---
+++ index.js
@@ -0,0 +1,15 @@
+const nom = document.getElementById("nom");
+const email = document.getElementById("email");
+const password = document.getElementById("password");
+const button = document.getElementById("button");
+
+button.addEventListener("click", function(){
+
+    if(nom.value == "" || email.value == "" || password.value == ""){
+        alert("Les champs sont vides");
+    }
+    else{
+        alert("Inscription réussie " + nom.value);
+    }
+
+});
