72218413aa5c9d5188fd0ee7514a0f923a608a29
Aya Elmaliky <ayaelmalki2004@gmail.com>
2026-05-16 14:58:39 +0200

    Ajout des fichiers

--- index.js
+++ index.js
@@ -2,13 +2,40 @@ const nom = document.getElementById("name");
 const email = document.getElementById("email");
 const password = document.getElementById("password");
 const button = document.getElementById("button");
+const message = document.getElementById("message");
 
 button.addEventListener("click", function () {
 
-    if (nom.value === "" || email.value === "" || password.value === "") {
-        alert("Les champs sont vides");
-    } else {
-        alert("Inscription réussie " + nom.value);
+    const nomValue = nom.value;
+    const emailValue = email.value;
+    const passwordValue = password.value;
+
+    // Vérifier les champs
+    if (nomValue === "" || emailValue === "" || passwordValue === "") {
+        message.textContent = "Veuillez remplir tous les champs";
+        message.style.color = "red";
+        return;
     }
 
+    // Récupérer users depuis localStorage
+    const users = JSON.parse(localStorage.getItem("users")) || [];
+
+    // Ajouter user جديد
+    users.push({
+        nom: nomValue,
+        email: emailValue,
+        password: passwordValue
+    });
+
+    
+    localStorage.setItem("users", JSON.stringify(users));
+
+    
+    message.textContent = "Inscription réussie !";
+    message.style.color = "green";
+
+   
+    nom.value = "";
+    email.value = "";
+    password.value = "";
 });
